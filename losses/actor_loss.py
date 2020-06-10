052fd20e18f46752c3a4844834749de4b314056f
Lucas Florin <lucasflorin4@gmail.com>
2020-06-10 16:22:20 +0200

    actor loss alpha parameter as init parameter

--- losses/actor_loss.py
+++ losses/actor_loss.py
@@ -3,11 +3,12 @@ import torch.nn as nn
 
 
 class ActorLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, alpha=1.0):
         super().__init__()
+        self.alpha = alpha
 
-    def forward(self, state_action_values, log_likelihoods, alpha=1.0):
-        return - (state_action_values + alpha * log_likelihoods)
+    def forward(self, state_action_values, log_likelihoods):
+        return - (state_action_values + self.alpha * log_likelihoods).mean()
 
 
 
