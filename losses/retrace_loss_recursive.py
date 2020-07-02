370dac8da2718b5cfad6ee38143cc2767b594068
Lucas Florin <lucasflorin4@gmail.com>
2020-07-02 17:50:04 +0200

    Implemented recursive Retrace loss as defined in ACER paper.

--- losses/retrace_loss_recursive.py
+++ losses/retrace_loss_recursive.py
@@ -11,7 +11,7 @@ class RetraceLossRecursive(nn.Module):
     """
 
     def __init__(self,
-                 gamma=0.95, use_gpu=False):
+                 gamma=0.99, use_gpu=False):
         super().__init__()
         self.gamma = gamma
         self.distance = nn.SmoothL1Loss()
@@ -20,28 +20,28 @@ class RetraceLossRecursive(nn.Module):
     def forward(self,
                 state_trajectory_action_values,
                 target_state_trajectory_action_values,
-                target_state_current_action_values,
+                target_expected_state_values,
                 rewards,
                 original_log_trajectory_action_probs,
                 target_log_trajectory_task_action_probs):
 
-        num_steps = state_trajectory_action_values.shape[0]
-        num_intentions = state_trajectory_action_values.shape[1]
-        log_importance_weights = target_log_trajectory_task_action_probs - original_log_trajectory_action_probs
-        importance_weights = torch.exp(torch.clamp(log_importance_weights, max=0))
-        padding = torch.zeros((1, num_intentions))
-        padding = padding.cuda() if self.use_gpu else padding
-        target_state_current_action_values = torch.cat([target_state_current_action_values[1:, :],
-                                                        padding], dim=0)
-        temporal_difference = (self.gamma * target_state_current_action_values
-                               - target_state_trajectory_action_values)
-        temporal_difference = rewards + temporal_difference
-        retrace_state_action_values = torch.zeros_like(state_trajectory_action_values)
+        state_trajectory_action_values = state_trajectory_action_values[:-1, :]
+        with torch.no_grad():
+            rewards = rewards[:-1, :]
+            target_state_trajectory_action_values = target_state_trajectory_action_values[1:, :]
+            target_expected_state_values = target_expected_state_values[1:, :]
+            num_steps = state_trajectory_action_values.shape[0]
+            log_importance_weights = target_log_trajectory_task_action_probs - original_log_trajectory_action_probs
+            importance_weights = torch.exp(torch.clamp(log_importance_weights, max=0))[1:, :]
+            retrace_state_action_values = torch.zeros_like(state_trajectory_action_values)
 
-        retrace_state_action_values[-1, :] = importance_weights[-1, :] * temporal_difference[-1, :]
-        for j in range(num_steps - 2, -1, -1):
-            retrace_state_action_values[j, :] = (importance_weights[j, :] * (temporal_difference[j, :]
-                                                 + self.gamma * retrace_state_action_values[j + 1, :]))
+            retrace_state_action_values[-1, :] = rewards[-1, :]
+            for j in range(num_steps - 2, -1, -1):
+                retrace_state_action_values[j, :] = (
+                        rewards[j, :]
+                        + self.gamma * (target_expected_state_values[j, :]
+                                        + importance_weights[j, :] * (retrace_state_action_values[j + 1, :]
+                                                                      - target_state_trajectory_action_values[j, :])))
 
         loss = self.distance(state_trajectory_action_values, retrace_state_action_values)
         return loss
