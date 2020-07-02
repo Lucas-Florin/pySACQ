3edf8f0dad2f2abe67475003e82a2131f9203ccf
Lucas Florin <lucasflorin4@gmail.com>
2020-07-02 16:32:09 +0200

    Fixed actor loss.
    Actor loss without entropy regularization by default.

--- losses/actor_loss.py
+++ losses/actor_loss.py
@@ -3,12 +3,12 @@ import torch.nn as nn
 
 
 class ActorLoss(nn.Module):
-    def __init__(self, alpha=1.0):
+    def __init__(self, alpha=0.0):
         super().__init__()
         self.alpha = alpha
 
     def forward(self, state_action_values, log_likelihoods):
-        return - (state_action_values + self.alpha * log_likelihoods).mean()
+        return (-state_action_values + self.alpha * log_likelihoods).mean()
 
 
 
